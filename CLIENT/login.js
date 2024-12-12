67f26251e5a791399c9788dc9f1f71eb6636452d
mansour1809 <mansour.mothna96@gmail.com>
2024-12-12 22:25:32 +0200

    Update login.js

--- CLIENT/login.js
+++ CLIENT/login.js
@@ -15,8 +15,10 @@ $(document).ready(() => {
       userAPI,
       JSON.stringify(user),
       (data) => {
+        console.log(data)
         localStorage.setItem("isLoggedIn", "true"); // Or sessionStorage
         localStorage.setItem("userName", user.userName);
+        localStorage.setItem("id", data);
         Swal.fire({
           title: "The user has been found!",
           text: "Login successful!",
@@ -53,50 +55,6 @@ $(document).ready(() => {
     });
   });
 });
-
-//        function showMessage(message, isError = false) {
-//     const messageDiv = document.getElementById('message');
-//     messageDiv.textContent = message;
-//     messageDiv.style.color = isError ? 'red' : 'green';
-// }
-
-// // קריאה ל-Login
-// document.getElementById('loginForm').addEventListener('submit', function (e) {
-//     e.preventDefault();
-
-//     // קבלת פרטי הטופס
-//     const username = document.getElementById('username').value;
-//     const password = document.getElementById('password').value;
-
-//     // קריאת Fetch לשרת
-//     fetch('/login', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({ username, password }),
-//     })
-//         .then(response => {
-//             if (!response.ok) {
-//                 return response.json().then(data => {
-//                     throw new Error(data.message);
-//                 });
-//             }
-//             return response.json();
-//         })
-//         .then(data => {
-//             showMessage(`Welcome, ${data.username}!`);
-//         })
-//         .catch(error => {
-//             showMessage(error.message, true);
-//         });
-// });
-
-// // קריאה ל-Sign Up
-// document.getElementById('signupButton').addEventListener('click', function () {
-//     // תוכל להוסיף פעולה נוספת אם צריך, לדוגמה לעבור לדף רישום אחר
-//     window.location.href = '/signup';  // לדוגמה, למעבר לדף רישום
-// });
 
 function ajaxCall(method, api, data, successCB, errorCB) {
   $.ajax({
